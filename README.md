abb2964bf17334c45173d6a679f09dde4c71df1c
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-04-15 05:22:14 +0100

    README & exercise steps in script

---
+++ README.md
@@ -0,0 +1,3 @@
+# w1-d4-exercise-js-oop
+
+1. Instructions directly in the script
--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Ready to go! 🚀");
+
+// 1. Create a car object with, at least, the following properties: brand, model
+// 2. Create a method that makes use of the 'this' keyword
+// 3. Convert the car object to a class named Car, with the same properties and method
+// 4. Try to create a new instance of the Car class
+// 5.1. Create a static property named 'numberOfCars' and set it to 0
+// 5.2 Increment the 'numberOfCars' property each time a new car is created
+// 5.3 Create a static method named 'getNumberOfCars' that returns the value of 'numberOfCars'
+// 6.1 Create a private property named '#owner' that stores the owner of the car
+// 6.2 Create a private method named '#getVIN' that returns a random string
+// 6.3 Create a method named 'getCarData' that returns the owner and the VIN
+// 7.1 Create a new class named ElectricCar that extends the Car class
+// 7.2 Add a new property named 'batteryKWh' to the ElectricCar class
+// 7.3 Create a method named 'getBatteryInfo' that returns the battery capacity
+// 7.4 Create a new instance of the ElectricCar class
+// 8. Review the prototype chain of the ElectricCar class
